0402926a68900e8faafffbaa5b87e3feb939af39
Damnjan Cosic <damnjan.cosic@prodyna.com>
2023-05-05 16:24:35 +0200

    Added CorsFilter

--- frontend/server-angular/src/app/service/server.service.ts
+++ frontend/server-angular/src/app/service/server.service.ts
@@ -1,23 +1,24 @@
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import { Injectable } from '@angular/core';
-import { Observable, throwError } from "rxjs";
-import { tap, catchError } from "rxjs/operators";
-import { CustomResponse } from '../interface/custom-response';
+import {Injectable} from '@angular/core';
+import {Observable, throwError} from "rxjs";
+import {tap, catchError} from "rxjs/operators";
+import {CustomResponse} from '../interface/custom-response';
 import {Server} from "../interface/server";
 import {Status} from "../enum/status.enum";
 
 @Injectable({providedIn: 'root'})
 export class ServerService {
-  private readonly apiUrl = 'any';
+  private readonly apiUrl = 'http://localhost:8080';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   servers$ = <Observable<CustomResponse>>
-  this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
-    .pipe(
-      tap(console.log),
-      catchError(this.handleError)
-    );
+    this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
+      .pipe(
+        tap(console.log),
+        catchError(this.handleError)
+      );
 
   save$ = (server: Server) => <Observable<CustomResponse>>
     this.http.post<CustomResponse>(`${this.apiUrl}/server/save`, server)
@@ -35,21 +36,23 @@ export class ServerService {
 
   filter$ = (status: Status, response: CustomResponse) => <Observable<CustomResponse>>
     new Observable<CustomResponse>(
-      suscriber => {
+      subscriber => {
         console.log(response);
-        suscriber.next(
-          status === Status.ALL ? { ...response, message: `Servers filtered by ${status} status`} :
+        subscriber.next(
+          status === Status.ALL ? {...response, message: `Servers filtered by ${status} status`} :
             {
               ...response,
               message: response.data.servers
-              .filter(server => server.status === status).length > 0 ? `Servers filtered by
-              ${status === Status.SERVER_UP ? 'SERVER UP' : 'SERVER DOWN'} status`
-                : `No servers of ${status} found`,
-              data: { servers: response.data.servers
-                  .filter(server => server.status === status) }
+                .filter(server => server.status === status).length > 0 ? `Servers filtered by
+              ${status === Status.SERVER_UP ? 'SERVER UP'
+                : 'SERVER DOWN'} status` : `No servers of ${status} found`,
+              data: {
+                servers: response.data.servers
+                  .filter(server => server.status === status)
+              }
             }
         );
-        suscriber.complete();
+        subscriber.complete();
       }
     )
       .pipe(
