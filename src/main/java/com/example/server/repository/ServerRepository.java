a35120f243e1b20a53be3a709b7465d5db828ea8
Damnjan Cosic <damnjan.cosic@prodyna.com>
2023-05-05 13:45:43 +0200

    First commit

---
+++ src/main/java/com/example/server/repository/ServerRepository.java
@@ -0,0 +1,8 @@
+package com.example.server.repository;
+
+import com.example.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepository extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
+}
